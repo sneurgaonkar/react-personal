c6afcee33e87316fcca50de3ab9a0ff76024069f
Sunil Neurgaonkar <sungame31@gmail.com>
2020-05-25 03:08:16 +0530

    Update yourdata.js

--- src/yourdata.js
+++ src/yourdata.js
@@ -2,18 +2,14 @@ export default
     {
         //(Please Do Not Remove The comma(,) after every variable)
         //Change The Website Template
-        name :'chetanverma.',
+        name :'sunil neurgaonkar',
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Your Name.',
-                        //Line 2 For Header
-                        'Digital designer ',
-                        //Line 3 For Header
-                        'from India'
+                        'Hi! 👋 I am Sunil.'
     ],
         //Contact Email
-        contactEmail:'hello@chetanverma.com',
+        contactEmail:'sunil@neurgaonkar.com',
         // Add Your About Text Here
-        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        abouttext: "Failing and learning at Whatfix. I am also hosting SaaS Sessions podcast out of my passion for SaaS. I believe that Growth is neither a process nor a hack. It's a mindset.",
         aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
