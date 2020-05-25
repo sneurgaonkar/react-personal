03c725955aaa9223da0c2894e71983fa408a7a55
Sunil Neurgaonkar <sungame31@gmail.com>
2020-05-25 11:40:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 import Header from './components/Header';
 import About from './components/About';
-import Work from './components/work';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar'
 
@@ -17,9 +16,6 @@ function App() {
       
       <div className='About'>
         <About></About>
-      </div>
-      <div className='work'>
-        <Work></Work>
       </div>
       <div className='contact'>
         <Contact></Contact>
